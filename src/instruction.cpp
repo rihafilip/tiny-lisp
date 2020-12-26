ee9931d7a63330c7ad8bd1a59bd8e324757c5903
Filip Riha <riha.fina@gmail.com>
2020-12-26 21:04:23 +0100

    Refactor Instruction::toString

--- src/instruction.cpp
+++ src/instruction.cpp
@@ -4,23 +4,25 @@ using namespace lisp::secd;
 
 std::string lisp::secd::toString ( Instruction ins )
 {
-	std::map<Instruction, std::string> map;
-	map . insert( std::make_pair(LDC, "LDC") );
-	map . insert( std::make_pair(NIL, "NIL") );
-	map . insert( std::make_pair(ADD, "ADD") );
-	map . insert( std::make_pair(SUB, "SUB") );
-	map . insert( std::make_pair(MUL, "MUL") );
-	map . insert( std::make_pair(DIV, "DIV") );
-	map . insert( std::make_pair(CONS, "CONS") );
-	map . insert( std::make_pair(CAR, "CAR") );
-	map . insert( std::make_pair(CDR, "CDR") );
-	map . insert( std::make_pair(CONSP, "CONSP") );
-	map . insert( std::make_pair(SEL, "SEL") );
-	map . insert( std::make_pair(JOIN, "JOIN") );
-	map . insert( std::make_pair(LD, " LD") );
-	map . insert( std::make_pair(LDF, "LDF") );
-	map . insert( std::make_pair(AP, " AP") );
-	map . insert( std::make_pair(RTN, "RTN") );
+	std::map<Instruction, std::string> map =
+	{
+		{ LDC, "LDC" },
+		{ NIL, "NIL" },
+		{ ADD, "ADD" },
+		{ SUB, "SUB" },
+		{ MUL, "MUL" },
+		{ DIV, "DIV" },
+		{ CONS, "CONS" },
+		{ CAR, "CAR" },
+		{ CDR, "CDR" },
+		{ CONSP, "CONSP" },
+		{ SEL, "SEL" },
+		{ JOIN, "JOIN" },
+		{ LD, " LD" },
+		{ LDF, "LDF" },
+		{ AP, " AP" },
+		{ RTN, "RTN" }
+	};
 
 	return map[ins];
 }
